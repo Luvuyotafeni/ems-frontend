f0f591b7ccabee02fc5ccba9d520e95e6194b4cf
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-05-28 19:06:20 +0200

    addtion of the routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,19 @@ import './App.css'
 import Footer from './components/Footer'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeesComponent from './components/ListEmployeesComponent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeesComponent/>
-      <Footer/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+            <Route path='/employees' element={<ListEmployeesComponent/>}></Route>
+          </Routes>
+        <Footer/>
+        </BrowserRouter>
     </>
   )
 }
