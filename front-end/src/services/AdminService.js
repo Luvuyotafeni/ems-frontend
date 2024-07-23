2fff2c3013627264ae4939a5646a00ab3609bde9
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-07-23 21:09:38 +0200

    added the delete admin api in the admin service

---
+++ front-end/src/services/AdminService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const Rest_Api_Base_Url = 'http://localhost:8080/api/admins';
+
+export const ListAdmins = () => axios.get(Rest_Api_Base_Url);
+
+export const createAdmin = (admin) => axios.post(Rest_Api_Base_Url, admin);
+
+export const getAdmin = (AdminId) => axios.get(Rest_Api_Base_Url +"/"+ AdminId);
+
+export const updateAdmin = (AdminId, Admin ) => axios.put(Rest_Api_Base_Url+"/"+ AdminId, Admin);
+
+export const deleteAdmin = (AdminId) => axios.delete(Rest_Api_Base_Url+"/"+AdminId);
